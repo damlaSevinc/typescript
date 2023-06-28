2ce8ae5ccf2c7cea08116c73feb090f0601d3911
Damla Sevinç Tokdemir <84271633+damlaSevinc@users.noreply.github.com>
2023-06-28 09:51:44 +0200

    Update challenge.ts - From Javascript to Typescript

--- challenge.ts
+++ challenge.ts
@@ -1,14 +1,22 @@
 // challenge.ts
-const prettyPrintWilder = (users) => {
-    users.map((user) => {
+
+interface User {
+    name: string;
+    age?: number;
+    birthday?: string;
+
+} 
+
+const prettyPrintWilder = (users: User[]) => {
+    users.map((user: User) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
   
   const wilders = [];
-  const user1 = { name: "Pierre", age: 23 };
-  const user2 = { name: "Paul", birthday: "10/02/1990" };
-  const user3 = { name: "Jacques", age: 25 };
+  const user1: User = { name: "Pierre", age: 23 };
+  const user2: User = { name: "Paul", birthday: "10/02/1990" };
+  const user3: User = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
   wilders.push(user3);
